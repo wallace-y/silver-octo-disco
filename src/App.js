3547c1e97571fa60af2a2d5a2f79daa1b2155e50
wallace-y <cameronwfinney@gmail.com>
2023-04-06 13:24:29 +0100

    changing layout of the form component and adding to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Code goes here</h1>
+      <Form/>
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,24 +3,31 @@ import React from "react";
 function Form() {
 
     return (
-        <main class="form-signin w-100 m-auto">
+      <div class="container w-50">
+        <main class="form-signin w-100 m-auto mt-3">
             <form>
-              <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+              <h1 class="h3 mb-3 fw-normal">Please register below</h1>
 
-              <div class="form-floating">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" placeholder="John"/>
+                <label for="floatingInput">First Name</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Smith"/>
+                <label for="floatingInput">Last Name</label>
+              </div>
+
+              <div class="form-floating mb-3">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
                 <label for="floatingInput">Email address</label>
               </div>
-              <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                <label for="floatingPassword">Password</label>
-              </div>
 
-
-              <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-              <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+              <button class="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+              <p class="mt-5 mb-3 text-body-secondary">© 2023</p>
             </form>
         </main>
+        </div>
     )
 }
 
